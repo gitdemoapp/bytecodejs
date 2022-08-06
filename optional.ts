37c2b366280036823c821c059da071f9bb8cce7a
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-08-06 20:30:56 +0530

    optional parameter

---
+++ optional.ts
@@ -0,0 +1,35 @@
+/*
+
+    Optional Parameters in functions:
+    --------------------------------------
+    function f1(fisrtname,lastname,middlename){
+         
+
+    }
+
+    1. While calling function we can make some parameters as optional.
+    2. introduced in ES6
+    3. we can represent optional parameters by using "?".
+    4. Optional parameters can not used in javascript environment.
+    5. Optional parameters works in type script
+    6. typescript is a programming language
+    7. it is given by microsoft.
+    8. it follows oops concept.
+    9. extension for typescript is ".ts".
+    10. browser does not undertsand typescript.
+    11. conversion from typescript to javascript is called transpilation.
+    12. tsc is a tool used for transpilation.
+    13. tsc stands for typescript compiler.
+
+
+*/
+
+function fun_one(fisrtname?:string,lastname?:string,middlename?:string){
+
+    console.log(fisrtname,lastname,middlename);
+
+}
+
+fun_one();
+
+//fun_one("raj");
